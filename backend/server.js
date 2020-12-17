cb58f4ee5c291289eb202fa64c5c908474348fad
saqibpsm <mh.saqibpsm@gmail.com>
2020-12-17 23:59:15 +0500

    custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import colors from 'colors';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
@@ -10,11 +11,11 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col } from "react-bootstrap";
-import Product from "../components/Product";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+import axios from 'axios';
 const HomeScreen = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
+      const { data } = await axios.get('/api/products');
       console.log(data.products);
       setProducts(data.products);
     };
@@ -18,7 +18,7 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {products.map(product => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
