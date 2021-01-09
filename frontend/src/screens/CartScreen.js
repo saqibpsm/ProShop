48737156fdf8d3b56548f0d709d5c28f13834771
saqibpsm <mh.saqibpsm@gmail.com>
2021-01-09 08:07:29 +0500

    Remove Items From Cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,7 +11,7 @@ import {
   Card,
 } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -28,8 +28,8 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty]);
 
-  const removeFromCartHandler = () => {
-    console.log('remove');
+  const removeFromCartHandler = id => {
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
