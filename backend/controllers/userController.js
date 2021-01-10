c7f12ee6b16875b9a672c726bfa52a4d5d736211
saqibpsm <mh.saqibpsm@gmail.com>
2021-01-10 16:55:05 +0500

    User Registration and password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,6 +24,35 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Register a new User
+// @route   POST /api/users
+// @access  public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already Exists');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
+  }
+});
+
 // @desc    Get User Profile
 // @route   GET /api/users/profile
 // @access  private
@@ -43,4 +72,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
